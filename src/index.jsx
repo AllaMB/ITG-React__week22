c57ebb4c8260707b7c29f613546eace6fd754cb6
AllaMB <alla.boussiron@gmail.com>
2023-10-30 22:03:42 +0100

    initiated React

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { App } from './App'
 import './assets/global.scss';
 
 
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+root.render(<App />);
